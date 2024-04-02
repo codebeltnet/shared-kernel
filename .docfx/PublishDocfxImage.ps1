66bef6168a9da767bddf0505754f2a9e6d0f342a
Michael Mortensen <mmdevelopment@hotmail.com>
2024-04-02 23:19:21 +0200

    Cleanup and typo fix

--- .docfx/PublishDocfxImage.ps1
+++ .docfx/PublishDocfxImage.ps1
@@ -1,3 +1,3 @@
 $version = minver -i
-docker tag savvyio-docfx:$version jcr.codebelt.net/geekle/sharedkernel-docfx:$version
+docker tag sharedkernel-docfx:$version jcr.codebelt.net/geekle/sharedkernel-docfx:$version
 docker push jcr.codebelt.net/geekle/sharedkernel-docfx:$version
