9bcf068d02bf0ebeeb185d329ba12090393d8c29
Michael Mortensen <michael@geekle.io>
2024-09-08 21:05:58 +0200

    :arrow_up: bumped dependencies

--- .docfx/PublishDocfxImage.ps1
+++ .docfx/PublishDocfxImage.ps1
@@ -1,3 +1,3 @@
-$version = minver -i
+$version = minver -i -t v -v w
 docker tag sharedkernel-docfx:$version jcr.codebelt.net/geekle/sharedkernel-docfx:$version
 docker push jcr.codebelt.net/geekle/sharedkernel-docfx:$version
